00194f92d5af13b14224654940dd56a11beb9d14
Tashfiq Akhand <tashfiqakhand@gmail.com>
2020-06-09 22:57:02 -0400

    SEG3125-Lab3

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -1,86 +1,153 @@
 // Array of products, each product is an object with different fieldset
 // A set of ingredients should be added to products		 
 
+// var img = document.creatElement("img");
+// var img = {
+// 	height:'20px',
+// 	width:'20px' 
+// }
+
+
 var products = [
 	{
-		name: "brocoli",
+		name: "broccoli",
 		vegetarian: true,
 		glutenFree: true,
-		both: true,
+		// both: true,
 		organic: true,
-		price: 1.99
+		price: 1.99,
+
+		Vegetable: true,
+		Fruit: false,
+		Meat: false,
+		WFD: false,
+		image: "Images/brocoli.jpg"
 	},
 	{
 		name: "carrot",
 		vegetarian: true,
 		glutenFree: true,
-		both: true,
+		// both: true,
 		organic: true,
-		price: 2.19
+		price: 2.19,
+
+		Vegetable: true,
+		Fruit: false,
+		Meat: false,
+		WFD: false,
+		image: "Images/carrot.jpg"
 	},
 	{
 		name: "bread",
 		vegetarian: true,
 		glutenFree: false,
-		both: false,
+		// both: false,
 		organic: false,
-		price: 2.35
+		price: 2.35,
+
+		Vegetable: false,
+		Fruit: false,
+		Meat: false,
+		WFD: true,
+		image: "Images/bread.jpg"
 	},
 	{
 		name: "banana",
 		vegetarian: true,
 		glutenFree: true,
-		both: true,
+		// both: true,
 		organic: false,
-		price: 2.79
+		price: 2.79,
+
+		Vegetable: false,
+		Fruit: true,
+		Meat: false,
+		WFD: false,
+		image: "Images/banana.jpg"
 	},
 	{
 		name: "apple",
 		vegetarian: true,
 		glutenFree: true,
-		both: true,
+		// both: true,
 		organic: true,
-		price: 2.99
+		price: 2.99,
+
+		Vegetable: false,
+		Fruit: true,
+		Meat: false,
+		WFD: false,
+		image: "Images/apple.jpg"
+	},
+	{
+		name: "croissant",
+		vegetarian: true,
+		glutenFree: false,
+		// both: false,
+		organic: false,
+		price: 3.35,
+
+		Vegetable: false,
+		Fruit: false,
+		Meat: false,
+		WFD: true,
+		image: "Images/croissant.jpg"
 	},
 	{
 		name: "pasta",
 		vegetarian: true,
 		glutenFree: false,
-		both: false,
+		// both: false,
 		organic: false,
-		price: 4.99
+		price: 4.99,
+
+		Vegetable: false,
+		Fruit: false,
+		Meat: false,
+		WFD: true,
+		image: "Images/pasta.jpg"
 	},
 	{
 		name: "chicken",
 		vegetarian: false,
 		glutenFree: true,
-		both: false,
+		// both: false,
 		organic: false,
-		price: 6.79
-	},
-	{
-		name: "cereal",
-		vegetarian: true,
-		glutenFree: false,
-		both: false,
-		organic: false,
-		price: 6.89
+		price: 6.79,
+
+		Vegetable: false,
+		Fruit: false,
+		Meat: true,
+		WFD: false,
+		image: "Images/chicken.jpg"
 	},
 	{
 		name: "salmon",
 		vegetarian: false,
 		glutenFree: true,
-		both: false,
+		// both: false,
 		organic: false,
-		price: 10.00
+		price: 10.00,
+
+		Vegetable: false,
+		Fruit: false,
+		Meat: true,
+		WFD: false,
+		image: "Images/salmon.jpg"
 	},
 	{
 		name: "steak",
 		vegetarian: false,
 		glutenFree: true,
-		both: false,
+		// both: false,
 		organic: false,
-		price: 12.45
+		price: 12.45,
+
+		Vegetable: false,
+		Fruit: false,
+		Meat: true,
+		WFD: false,
+		image: "Images/steak.jpg"
 	}
 ];
 	
@@ -93,29 +160,36 @@ function restrictListProducts(prods, restriction) {
 	let product_names = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
-			product_names.push(prods[i].name + " " + prods[i].price);
+			product_names.push(prods[i]);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
-			product_names.push(prods[i].name+ " " +prods[i].price);
-		}
-		else if ((restriction == "Both") && (prods[i].both == true)){
-			product_names.push(prods[i].name+ " " +prods[i].price);
+			product_names.push(prods[i]);
 		}
 		else if ((restriction == "Organic") && (prods[i].organic == true)){
-			product_names.push(prods[i].name+ " " +prods[i].price);
+			product_names.push(prods[i]);
 		}
 		else if (restriction == "None"){
-			product_names.push(prods[i].name+ " " +prods[i].price);
+			product_names.push(prods[i]);
+		}
+		else if ((restriction == "Vegetarian & GlutenFree") && (prods[i].vegetarian == true) && (prods[i].glutenFree == true)){
+			product_names.push(prods[i]);
+		}
+		else if ((restriction == "Vegetarian & Organic") && (prods[i].vegetarian == true) && (prods[i].organic == true)){
+			product_names.push(prods[i]);
+		}
+		else if ((restriction == "GlutenFree & Organic") && (prods[i].glutenFree == true) && (prods[i].organic == true)){
+			product_names.push(prods[i]);
 		}
 	}
 	return product_names;
 }
 
+
 // Calculate the total price of items, with received parameter being a list of products
 function getTotalPrice(chosenProducts) {
 	totalPrice = 0;
 	for (let i=0; i<products.length; i+=1) {
-		if (chosenProducts.indexOf(products[i].name+ " " + products[i].price) > -1){
+		if (chosenProducts.indexOf(prods[i]) > -1){
 			totalPrice += products[i].price;
 		}
 	}
--- scripts/main.js
+++ scripts/main.js
@@ -21,20 +21,86 @@ function openInfo(evt, tabName) {
 
 }
 
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+var bcc = document.getElementsByClassName("panel");
+function toggle() {
+	for (i = 0; i < acc.length; i++) {
+		if(acc[i].className === "accordion active") {
+			acc[i].className = "accordion";
+			bcc[i].style.display = "none";
+		}
+	}
+}
+
+function ProductOrganization() {
+	var w = document.getElementById("None").checked;
+	var x = document.getElementById("Vegetarian").checked;
+	var y = document.getElementById("GlutenFree").checked;
+	var z = document.getElementById("Organic").checked;
+	if (x == true && y == true) {
+		populateListProductChoices("Vegetarian & GlutenFree", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else if (x == true && z == true) {
+		populateListProductChoices("Vegetarian & Organic", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else if (y == true && z == true) {
+		populateListProductChoices("GlutenFree & Organic", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else if (x == true) {
+		populateListProductChoices("Vegetarian", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else if (y == true) {
+		populateListProductChoices("GlutenFree", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else if (z == true) {
+		populateListProductChoices("Organic", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else if (w == true) {
+		populateListProductChoices("None", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+	else{
+		populateListProductChoices("None", 'displayVegetable', 'displayFruit', 'displayMeat', 'displayWFD')
+	}
+}
 
 	
 // generate a checkbox list from a list of products
 // it makes each product name as the label for the checkbos
 
-function populateListProductChoices(slct1, slct2) {
-    var s1 = document.getElementById(slct1);
+function populateListProductChoices(slct1, slct2, slct3, slct4, slct5) {
+
+	var s1 = slct1;
     var s2 = document.getElementById(slct2);
-	
-	// s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+	var s3 = document.getElementById(slct3);
+	var s4 = document.getElementById(slct4);
+	var s5 = document.getElementById(slct5);
     s2.innerHTML = "";
+	s3.innerHTML = "";
+	s4.innerHTML = "";
+	s5.innerHTML = "";
+
+
+ //    var s1 = document.getElementById(slct1);
+ //    var s2 = document.getElementById(slct2);
+	
+	// // s2 represents the <div> in the Products tab, which shows the product list, so we first set it empty
+ //    s2.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+    var optionArray = restrictListProducts(products, s1);
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
@@ -43,23 +109,71 @@ function populateListProductChoices(slct1, slct2) {
 	for (i = 0; i < optionArray.length; i++) {
 			
 		var productName = optionArray[i];
+
+
+		var image = productName.image;
+		
 		// create the checkbox and add in HTML DOM
 		var checkbox = document.createElement("input");
 		checkbox.type = "checkbox";
 		checkbox.name = "product";
-		checkbox.value = productName;
-		s2.appendChild(checkbox);
-		
-		// create a label for the checkbox, and also add in HTML DOM
-		var label = document.createElement('label')
-		label.htmlFor = productName;
-		label.appendChild(document.createTextNode(productName));
-		s2.appendChild(label);
-		
-		// create a breakline node and add in HTML DOM
-		s2.appendChild(document.createElement("br"));    
+		checkbox.value = productName.name;
+
+		var picture = document.createElement("IMG");
+		picture.setAttribute("src", image);
+		picture.setAttribute("width", "50");
+ 		picture.setAttribute("height", "50");
+
+
+
+ 		var label = document.createElement('label')
+		label.htmlFor = productName.name;
+		label.appendChild(document.createTextNode(productName.name + " " + productName.price));
+
+		if(productName.Vegetable == true) {
+			s2.appendChild(checkbox);
+			s2.appendChild(picture);
+			s2.appendChild(document.createElement("br"));
+			s2.appendChild(checkbox);
+			s2.appendChild(label);
+			s2.appendChild(document.createElement("br"));
+		}
+		else if(productName.Fruit == true) {
+			s2.appendChild(checkbox);
+			s3.appendChild(picture);
+			s3.appendChild(document.createElement("br"));
+			s3.appendChild(checkbox);
+			s3.appendChild(label);
+			s3.appendChild(document.createElement("br"));
+		}
+		else if(productName.Meat == true) {
+			s2.appendChild(checkbox);
+			s4.appendChild(picture);
+			s4.appendChild(document.createElement("br"));
+			s4.appendChild(checkbox);
+			s4.appendChild(label);
+			s4.appendChild(document.createElement("br"));
+		}
+		else if(productName.WFD == true){
+			s2.appendChild(checkbox);
+			s5.appendChild(picture);
+			s5.appendChild(document.createElement("br"));
+			s5.appendChild(checkbox);
+			s5.appendChild(label);
+			s5.appendChild(document.createElement("br"));
+		}
+		else{
+			s2.appendChild(checkbox);
+			s5.appendChild(picture);
+			s5.appendChild(document.createElement("br"));
+			s5.appendChild(checkbox);
+			s5.appendChild(label);
+			s5.appendChild(document.createElement("br"));
+		}
 	}
 }
+
+
 	
 // This function is called when the "Add selected items to cart" button in clicked
 // The purpose is to build the HTML to be displayed (a Paragraph) 
